78415a10ca50c6165565891ae998028d60bf1636
Greg Barker <grbarker04@gmail.com>
2022-02-10 11:45:30 -0800

    Changed the main view to SafeAreaView.

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View, Button, Image, Modal, Alert } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, Button, Image, Modal, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.innerContainer}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changed it to a class component</Text>
         <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  innerContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
